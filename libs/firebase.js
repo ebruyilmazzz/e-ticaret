27c74d727d92196ca3c4e6eacc8b8678e13eed20
ebruyilmazzz <154504631+ebruyilmazzz@users.noreply.github.com>
2025-02-25 11:37:15 +0300

    Update firebase.js

--- libs/firebase.js
+++ libs/firebase.js
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD1c8x8A9V_YX7483eUswG-4b5919YoR90",
-  authDomain: "shop-accdf.firebaseapp.com",
-  projectId: "shop-accdf",
-  storageBucket: "shop-accdf.appspot.com",
-  messagingSenderId: "1055750601136",
-  appId: "1:1055750601136:web:087162ab8982c958c9be75",
-  measurementId: "G-6X1ELM8NQK"
+  apiKey: ,
+  authDomain: ,
+  projectId:,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: ,
+  measurementId:
 };
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
